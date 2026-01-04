ea97b7bc69ece53a2a07917dac7765fcbbc4f8f5
FelipeZapata137 <felipe.zapata@epn.edu.ec>
2026-01-04 16:28:29 -0500

    Tiempo de carousel disminuido a 2 seg

--- src/components/shop/AntigrasaCarouselCard.jsx
+++ src/components/shop/AntigrasaCarouselCard.jsx
@@ -8,7 +8,7 @@ export default function AntigrasaCarouselCard({ products, onCotizar }) {
   useEffect(() => {
     const timer = setInterval(() => {
       setCurrentIndex((prev) => (prev + 1) % products.length)
-    }, 2500)
+    }, 2000)
 
     return () => clearInterval(timer)
   }, [products.length])
